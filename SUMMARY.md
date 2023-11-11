87092f505e311635fb0bc00fb1f832c05b295951
crimson <2272650008@qq.com>
2023-11-11 23:27:13 +0800

    first

---
+++ SUMMARY.md
@@ -0,0 +1,8 @@
+# SUMMARY
+
+* [Introduction](README.md)
+
+# 概要
+
+- [第一章](docs/gitbook_manual/index.md)
+  - [1.1 第一节](docs/gitbook_manual/环境配置/text.md)
---
+++ book.js
@@ -0,0 +1,18 @@
+module.exports = {
+  // 书籍信息
+  title: "黑眼圈手册",
+  description: "还没想好",
+  author: "黑眼圈小组",
+  lang: "zh-cn",
+
+  // 插件列表
+  plugins: ["-lunr", "-search", "search-pro2","code"],
+
+  // 插件全局配置
+  pluginsConfig: {},
+
+  // 模板变量
+  variables: {
+    // 自定义
+  },
+};
